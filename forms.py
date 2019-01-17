d2493b1cd6822da04646a71fd4814ca2bcbe7dc1
toufanialfarisi <toufani.deeplearning@gmail.com>
2019-01-17 14:16:31 +0800

    fix code

--- forms.py
+++ forms.py
@@ -1,9 +1,20 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
-    first_name = StringField('First name')
-    last_name = StringField('Last name')
-    email = StringField('Email')
-    password = PasswordField('Password')
+    first_name = StringField('First name', validators=[DataRequired("silahkan masukan nama pertama")])
+    last_name = StringField('Last name', validators=[DataRequired("silahkan masukan nama terakhir")])
+    email = StringField('Email', validators=[DataRequired("silahkan masukan email"), Email("give correct email !")])
+    password = PasswordField('Password', validators=[DataRequired("silahkan masukan password"), Length(min=6, message="password must be 6 characters")])
     submit = SubmitField('Sign up')
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired("please enter your email address."), Email("please enter your email again.")])
+    password = PasswordField('password', validators=[DataRequired("Please enter a password")])
+    submit = SubmitField("sign in")
+
+class AddressForm(Form):
+    address = StringField('Address', validators=[DataRequired('please enter an address.')])
+    submit = SubmitField('Search')
+
--- routes.py
+++ routes.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
-from models import db
-from forms import SignupForm
+from flask import Flask, render_template, request, session, url_for, redirect
+from models import db, User
+from forms import SignupForm, LoginForm, AddressForm
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMU_DATABASE_URI'] ='postgresql://127.0.0.1/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql:///learningflask'
 db.init_app(app)
 
 app.secret_key = "development-key"
@@ -18,10 +18,72 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
+
+    if 'email' in session:
+        return redirect(url_for('home'))
+
     form = SignupForm()
-    return render_template('signup.html', form=form)
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
+        else:
+
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            # session['email'] = newuser.email 
+            # return redirect(url_for('home'))
+            return redirect(url_for('login'))
+
+    elif request.method == 'GET':
+        return render_template('signup.html', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+
+    if 'email' in session:
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template("login.html", form=form)
+        else:
+            email = form.email.data
+            password = form.password.data
+            
+            user = User.query.filter_by(email=email).first()
+            if user is not None and user.check_password(password):
+                session['email'] = form.email.data
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('login'))
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
+
+
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+@app.route("/home")
+def home():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+    # return render_template("home.html")
+    
+    form = AddressForm()
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('home.html', form=form)
+        else:
+            pass
+    elif request.method == 'GET':
+        return render_template('home.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
