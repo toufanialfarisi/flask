9c2f3d009ed50f0b92e491b396314cb6b6195905
toufanialfarisi <toufani.deeplearning@gmail.com>
2019-01-17 08:59:23 +0800

    route to signup

--- routes.py
+++ routes.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from models import db
+from forms import SignupForm
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMU_DATABASE_URI'] ='postgresql://127.0.0.1/learningflask'
+db.init_app(app)
+
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -9,6 +17,11 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/signup")
+def signup():
+    form = SignupForm()
+    return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
